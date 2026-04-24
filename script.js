f42907b2c1de873b93040c61a274ea82043e5059
hajiraxxx <hajira.ali103@gmail.com>
2026-04-24 21:00:36 +0100

    script.js

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-body {
-    background: pink;
-    font-family: Arial;
-    display: flex;
-    justify-content: center;
-    margin-top: 50px;
+function openMessages() {
+    document.getElementById("home").classList.add("hidden");
+    document.getElementById("messages").classList.remove("hidden");
 }
 
-.phone {
-    width: 250px;
-    height: 450px;
-    background: hotpink;
-    border-radius: 30px;
-    padding: 15px;
-    box-shadow: 0 0 20px white;
-}
-
-.screen {
-    background: white;
-    height: 100%;
-    border-radius: 20px;
-    padding: 10px;
-}
-
-.hidden {
-    display: none;
-}
-
-button {
-    background: pink;
-    border: none;
-    padding: 10px;
-    border-radius: 10px;
-    margin-top: 10px;
-}
-
-.msg {
-    background: #ffe6f0;
-    padding: 5px;
-    border-radius: 10px;
-    margin: 5px 0;
+function goHome() {
+    document.getElementById("messages").classList.add("hidden");
+    document.getElementById("home").classList.remove("hidden");
 }
