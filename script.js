13e0a96c3439617da0573d1f0c87ea4b02d27231
hajiraxxx <hajira.ali103@gmail.com>
2026-04-24 20:59:53 +0100

    script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+body {
+    background: pink;
+    font-family: Arial;
+    display: flex;
+    justify-content: center;
+    margin-top: 50px;
+}
+
+.phone {
+    width: 250px;
+    height: 450px;
+    background: hotpink;
+    border-radius: 30px;
+    padding: 15px;
+    box-shadow: 0 0 20px white;
+}
+
+.screen {
+    background: white;
+    height: 100%;
+    border-radius: 20px;
+    padding: 10px;
+}
+
+.hidden {
+    display: none;
+}
+
+button {
+    background: pink;
+    border: none;
+    padding: 10px;
+    border-radius: 10px;
+    margin-top: 10px;
+}
+
+.msg {
+    background: #ffe6f0;
+    padding: 5px;
+    border-radius: 10px;
+    margin: 5px 0;
+}
