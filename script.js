e28da2340bb178ed2dd64c3350441d970d3ffdca
hajiraxxx <hajira.ali103@gmail.com>
2026-04-24 21:33:21 +0100

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+function enterChat() {
+    let name = document.getElementById("username").value;
+    if (!name.trim()) return;
+
+    document.getElementById("user-display").innerText = name;
+
+    document.getElementById("login").classList.add("hidden");
+    document.getElementById("chat-screen").classList.remove("hidden");
+}
+
 function sendMessage() {
     let input = document.getElementById("message-input");
     let text = input.value;
 
-    if (text === "") return;
+    if (!text.trim()) return;
 
     let msg = document.createElement("div");
     msg.classList.add("message", "sent");
@@ -11,4 +21,7 @@ function sendMessage() {
     document.getElementById("chat-box").appendChild(msg);
 
     input.value = "";
+
+    document.getElementById("chat-box").scrollTop =
+        document.getElementById("chat-box").scrollHeight;
 }
